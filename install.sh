6afeac2863f1a53681a33cb63dd7cdcd39126c2a
Dan Medeiros <dan.medeiros@sagewell.com>
2024-04-20 08:29:12 +0000

    Adjusts for Debian 12

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installer can only be executed once.
+# Installer must be run with sudo or as root.
 if [ "$EUID" -ne 0 ]; then
   echo "This script must be run with sudo or as root."
   exit 1
